7c7fe9f731a6c619c7003587c273de6246666d9c
mlade <ladelz86@hotmail.com>
2021-08-27 17:23:39 +1000

    removed request import from main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, url_for, flash, request, abort, session
+from flask import Flask, render_template, redirect, url_for, flash, abort, session
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
