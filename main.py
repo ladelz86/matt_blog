7e5c6aeb1b80ed8dd62cc807552197ba19e85d92
mlade <ladelz86@hotmail.com>
2021-08-27 17:41:15 +1000

    changed mail to yahoo

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # Mail Settings
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_SERVER"] = "smtp.mail.yahoo.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ.get("GMAIL_ADDR")
